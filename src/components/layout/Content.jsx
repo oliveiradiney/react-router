70bd0ec07df495f1c38b768576ca5e52599a03d0
oliveiradiney <dineyoliveira1999@gmail.com>
2021-12-23 13:22:49 -0300

    Página não encontrada

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -5,6 +5,7 @@ import { Switch, Route } from "react-router-dom";
 import About from '../../views/examples/About'
 import Home from '../../views/examples/Home'
 import Param from '../../views/examples/Param';
+import NotFound from '../../views/examples/NotFound';
 
 const Content = props => (
     <main className="Content">
@@ -18,6 +19,9 @@ const Content = props => (
             </Route>
             <Route path='/param/:id'>
                 <Param />
+            </Route>
+            <Route path='*'>
+                <NotFound />
             </Route>
          
         </Switch>
